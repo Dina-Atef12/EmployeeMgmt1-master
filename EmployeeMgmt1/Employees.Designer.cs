5a73d6d3d03e79bd7d2f77f1400afdbfbd811d1b
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 17:02:21 +0200

    DeleteBtn.Text = "Delete

--- EmployeeMgmt1/Employees.Designer.cs
+++ EmployeeMgmt1/Employees.Designer.cs
@@ -405,7 +405,7 @@
             this.DeleteBtn.Name = "DeleteBtn"; 
             this.DeleteBtn.Size = new System.Drawing.Size(116, 35);
             this.DeleteBtn.TabIndex = 56;
-            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.Text = "Delete"; 
             this.DeleteBtn.UseVisualStyleBackColor = false;
             this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
             // 
