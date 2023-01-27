c17171031c75020a4c3ca458591e19f990ef811c
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 17:19:43 +0200

    if statement

--- EmployeeMgmt1/login.cs
+++ EmployeeMgmt1/login.cs
@@ -34,7 +34,7 @@ namespace EmployeeMgmt1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(UNameTb.Text == "" || PasswordTb.Text == "")
+            if(UNameTb.Text == "" || PasswordTb.Text == "") 
             {
                 MessageBox.Show("Missing Data!!!");
             }
