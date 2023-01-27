9b2caf6cf80326afd706fbd41a966352e10d2896
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 16:59:33 +0200

    label11.Text = "Departments

--- EmployeeMgmt1/Employees.Designer.cs
+++ EmployeeMgmt1/Employees.Designer.cs
@@ -311,7 +311,7 @@
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(134, 25);
             this.label11.TabIndex = 50;
-            this.label11.Text = "Departments";
+            this.label11.Text = "Departments"; 
             // 
             // pictureBox3
             // 
