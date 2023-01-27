215bdd8a9e833ff1e05cfd03389af85be285b05d
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 18:13:45 +0200

    Obj.Show()

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -196,7 +196,7 @@ namespace EmployeeMgmt1
         private void label9_Click(object sender, EventArgs e)
         {
             login Obj = new login();  
-            Obj.Show();
+            Obj.Show(); 
             this.Hide();
         }
 
