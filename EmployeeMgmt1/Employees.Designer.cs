f2465c702bea24458647685d5988436684a349cd
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 16:30:18 +0200

    if statement

--- EmployeeMgmt1/Employees.Designer.cs
+++ EmployeeMgmt1/Employees.Designer.cs
@@ -12,8 +12,8 @@
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
+        { 
+            if (disposing && (components != null)) 
             {
                 components.Dispose();
             }
