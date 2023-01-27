c9e5ebaf70cbdc1295c859d94168c963e821744d
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 18:02:02 +0200

    string DOB = DOBTb.Value.Date.ToString()

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -155,7 +155,7 @@ namespace EmployeeMgmt1
                     string Name = EmpNameTb.Text; 
                     string Gender = GenCb.SelectedItem.ToString(); 
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString()); 
-                    string DOB = DOBTb.Value.Date.ToString();
+                    string DOB = DOBTb.Value.Date.ToString(); 
                     string JDate = JDateTb.Value.Date.ToString();
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
