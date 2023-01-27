e30800b9a5085fe36a137f99f6eadf38da54e186
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 18:01:15 +0200

    string Gender = GenCb.SelectedItem.ToString()

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -153,7 +153,7 @@ namespace EmployeeMgmt1
                 else
                 {
                     string Name = EmpNameTb.Text; 
-                    string Gender = GenCb.SelectedItem.ToString();
+                    string Gender = GenCb.SelectedItem.ToString(); 
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
                     string DOB = DOBTb.Value.Date.ToString();
                     string JDate = JDateTb.Value.Date.ToString();
