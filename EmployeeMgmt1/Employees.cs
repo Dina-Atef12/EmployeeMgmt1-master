897677dad4417395379f14dcfdabbfccbbf65478
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 17:59:38 +0200

    if statement

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -144,9 +144,9 @@ namespace EmployeeMgmt1
 
         private void UpdateBtn_Click(object sender, EventArgs e) 
         {
-            try
+            try 
             {
-                if (EmpNameTb.Text == "" || GenCb.SelectedIndex == -1 || DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
+                 if (EmpNameTb.Text == "" || GenCb.SelectedIndex == -1 || DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
                 {
                     MessageBox.Show("missing data!!!");
                 }
