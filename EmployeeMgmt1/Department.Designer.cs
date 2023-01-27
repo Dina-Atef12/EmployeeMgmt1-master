4e1dda32aaf4c1d12001adad9ab0dda537721463
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 15:18:11 +0200

    label1.Text = "Employee mangement system version 10"

--- EmployeeMgmt1/Department.Designer.cs
+++ EmployeeMgmt1/Department.Designer.cs
@@ -149,7 +149,7 @@ namespace EmployeeMgmt1
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(369, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Employee mangement system version 10";
+            this.label1.Text = "Employee mangement system version 10"; 
             // 
             // panel1
             // 
