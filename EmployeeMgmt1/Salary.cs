8679def6820b925829b2205c369f663d666bc6c4
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 17:33:29 +0200

    MessageBox.Show("Salary  Paid!!!");

--- EmployeeMgmt1/Salary.cs
+++ EmployeeMgmt1/Salary.cs
@@ -85,7 +85,7 @@ namespace EmployeeMgmt1
                     Query = string.Format(Query, EmpCb.SelectedValue.ToString(), Days, Period, Amount, DateTime.Today.Date);
                     Con.SetData(Query);
                     ShowSalary();
-                    MessageBox.Show("Salary  Paid!!!");
+                    MessageBox.Show("Salary  Paid!!!"); 
                     DaysTb.Text = "";
                 }
             }
